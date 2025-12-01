3ab1f94ff8319438d89c0985a83f0ece2567c237
OscarNeiraN <o.neira@duocuc.cl>
2025-12-01 12:18:47 -0300

    pipeline

--- vulnerable_flask_app.py
+++ vulnerable_flask_app.py
@@ -235,4 +235,4 @@ def admin():
 
 # Bandit-safe: no debug, no 0.0.0.0
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="127.0.0.1", port=5000, debug=False)
